ab2ec95065982b1bfd1c957ec84ec4d1d6579ca1
Paige Throckmorton <path4942@gmail.com>
2017-11-13 13:00:04 -0600

    Created css wheel for waiting for restaurants

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,13 +1,26 @@
-var zomato = require ('zomato');
-var client = zomato.createClient({
-  userKey:'a86a2828ab83989f3ed612fe6d185c1d'
+$(document).ready(function(){
+
+  $('#button').click(function(){
+    $('.loading-overlay').show();
+    setTimeout(function() {$('.loading-overlay').hide()}, 5000);
+  });
+
 });
 
-client.getCategories(null, function(err, result){
-  if(!err){
-    console.log(result);
-  }else {
-    console.log(err);
-  }
-});
-curl -X GET --header "Accept: application/json" --header "user-key: a86a2828ab83989f3ed612fe6d185c1d" "https://developers.zomato.com/api/v2.1/cities?q=austin"
+// var zomato = require ('zomato');
+// var client = zomato.createClient({
+//   userKey:'a86a2828ab83989f3ed612fe6d185c1d'
+// });
+//
+// var $xhr = $.getJSON(`https://developers.zomato.com/api/v2.1/cities?q=austin?key=userKey`)
+//
+//
+//
+//
+// function spinWheel(){
+//
+// }
+//
+// function getRestaurants(){
+//
+// }

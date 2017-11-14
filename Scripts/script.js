ef488a84d68209b3617a6bdd5620c8aac6ee1a35
Paige Throckmorton <path4942@gmail.com>
2017-11-14 11:32:16 -0600

    Added map, killed js and started over

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,55 +1,100 @@
-// let resButton = $("button")
-//
-//   resButton.click(function(){
-//     $('.loading-overlay').show();
-//     setTimeout(function() {$('.loading-overlay').hide()}, 5000);
-//   });
+(function() {
 
 
-const restaurants = [];
+  function initMap() {
+    let currentLocation;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      if (position) {
+        currentLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+      } else {
+        // Default to somewhere in Austin.
+        currentLocation = {
+          lat: -33.867,
+          lng: 151.206
+        }
+      }
+      configureMap(currentLocation);
+    });
+    function configureMap(currentLocation) {
+      map = new google.maps.Map(document.getElementById('foodMap'), {
+        center: currentLocation,
+        zoom: 15,
+        styles: [{
+          stylers: [{
+            visibility: 'simplified'
+          }]
+        }, {
+          elementType: 'labels',
+          stylers: [{
+            visibility: 'off'
+          }]
+        }]
+      });
 
+      infoWindow = new google.maps.InfoWindow();
+      service = new google.maps.places.PlacesService(map);
 
-let searchButton = $("button");
+      // The idle event is a debounced event, so we can query & listen without
+      // throwing too many requests at the server.
+      map.addListener('idle', performSearch);
+    }
 
-searchButton.click(function() {
-  event.preventDefault();
-  let searchText = $('#city').val();
-  console.log(searchText);
-  getMovies(searchText);
-
-});
-
-function getRestaurants(searchText) {
-  if (searchText.length === 0) {
-    alert('Search for a City')
-  } else {
-    clearListings();
   }
 
-var $xhr = $.getJSON(`https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+${searchText}&key=AIzaSyAb3b9RbnL7iVzNS_FncN7jLyRvxbop7yA`)
+  function performSearch(searchTerm) {
+    var request = {
+      bounds: map.getBounds(),
+      keyword: searchTerm
+    };
+    service.radarSearch(request, callback);
+  }
 
-  $xhr.done(function(data) {
-    let food = data["Search"]
-    for (let i = 0; i < films.length; i++) {
-      restaurants.push(food[i]);
-
+  function callback(results, status) {
+    if (status !== google.maps.places.PlacesServiceStatus.OK) {
+      console.error(status);
+      return;
     }
-    console.log($xhr);
+    for (var i = 0, result; result = results[i]; i++) {
+      //results.length.Random....
+      addMarker(result);
+    }
+  }
 
-  });
-}
+  function addMarker(place) {
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location,
+      icon: {
+        url: 'https://developers.google.com/maps/documentation/javascript/images/circle.png',
+        anchor: new google.maps.Point(10, 10),
+        scaledSize: new google.maps.Size(10, 17)
+      }
+    });
 
+    google.maps.event.addListener(marker, 'click', function() {
+      service.getDetails(place, function(result, status) {
+        if (status !== google.maps.places.PlacesServiceStatus.OK) {
+          console.error(status);
+          return;
+        }
+        infoWindow.setContent(result.name);
+        infoWindow.open(map, marker);
+      });
+    });
+  }
 
+  function configureSubmitEventListener() {
+    let submitButton = $(".btn btn-primary");
+    submitButton.click(function(){
+        var option_user_selection = document.getElementById("foodType").options[document.getElementById("foodType").selectedIndex ].text
+        performSearch(option_user_selection);
+    });
 
+  }
 
-// $.ajax({
-//   dataType: "json",
-//   url: 'https://cors-anywhere.herokuapp.com/developers.zomato.com/api/v2.1/cities?q=Austin'
-// ,
-//   data: 'Austin',
-//   success: function(){
-//     console.log('success')
-//   },
-//   headers:
-//   {user_key:'a86a2828ab83989f3ed612fe6d185c1d'}
-// });
+  initMap();
+  configureSubmitEventListener();
+})();

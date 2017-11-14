03b06ec6d647ca94b58946ec6c4b8bbbfc268d1e
Paige Throckmorton <path4942@gmail.com>
2017-11-13 21:47:37 -0600

    API calls

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,26 +1,55 @@
-$(document).ready(function(){
+// let resButton = $("button")
+//
+//   resButton.click(function(){
+//     $('.loading-overlay').show();
+//     setTimeout(function() {$('.loading-overlay').hide()}, 5000);
+//   });
 
-  $('#button').click(function(){
-    $('.loading-overlay').show();
-    setTimeout(function() {$('.loading-overlay').hide()}, 5000);
-  });
+
+const restaurants = [];
+
+
+let searchButton = $("button");
+
+searchButton.click(function() {
+  event.preventDefault();
+  let searchText = $('#city').val();
+  console.log(searchText);
+  getMovies(searchText);
 
 });
 
-// var zomato = require ('zomato');
-// var client = zomato.createClient({
-//   userKey:'a86a2828ab83989f3ed612fe6d185c1d'
+function getRestaurants(searchText) {
+  if (searchText.length === 0) {
+    alert('Search for a City')
+  } else {
+    clearListings();
+  }
+
+var $xhr = $.getJSON(`https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+${searchText}&key=AIzaSyAb3b9RbnL7iVzNS_FncN7jLyRvxbop7yA`)
+
+  $xhr.done(function(data) {
+    let food = data["Search"]
+    for (let i = 0; i < films.length; i++) {
+      restaurants.push(food[i]);
+
+    }
+    console.log($xhr);
+
+  });
+}
+
+
+
+
+// $.ajax({
+//   dataType: "json",
+//   url: 'https://cors-anywhere.herokuapp.com/developers.zomato.com/api/v2.1/cities?q=Austin'
+// ,
+//   data: 'Austin',
+//   success: function(){
+//     console.log('success')
+//   },
+//   headers:
+//   {user_key:'a86a2828ab83989f3ed612fe6d185c1d'}
 // });
-//
-// var $xhr = $.getJSON(`https://developers.zomato.com/api/v2.1/cities?q=austin?key=userKey`)
-//
-//
-//
-//
-// function spinWheel(){
-//
-// }
-//
-// function getRestaurants(){
-//
-// }
